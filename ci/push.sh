fc8e9f9fcaa7ae6207eceae10e1404305a1f2da6
chaan2835 <92902854+chaan2835@users.noreply.github.com>
2023-02-21 11:47:03 +0530

    Update push.sh

--- ci/push.sh
+++ ci/push.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-echo "/root/.jenkins/workspace/icici-funds/target/"
-sudo cd /root/.jenkins/workspace/icici-funds/target/
+echo "/home/ubuntu/.jenkins/workspace/icici-funds/target"
+sudo cd /home/ubuntu/.jenkins/workspace/icici-funds/target
 echo "ls -lrt"
-ls -lh 
+ls -lhrt 
 artifact_file_name=`ls *.war`
 echo "file name: $artifact_file_name"
 curl -u jenkins:Chandra@2835 -X PUT "http://65.2.177.248:8081/artifactory/libs-snapshot/$JOB_NAME/$artifact_file_name" -T $artifact_file_name
